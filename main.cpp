44de51b9e76ad3067408193067bba44ebf7bb8e3
korsar <kosart2006@gmail.com>
2025-02-16 18:40:18 +0300

    change main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-#include <cstring>
+#include "mymath/mymath.h"
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    // сюда решение
+    std::cout << "Я умею суммировать! Зацени 1+1=";
+    std::cout << mymath::sum(10, 32) << std::endl;
+
     return 0;
 }
