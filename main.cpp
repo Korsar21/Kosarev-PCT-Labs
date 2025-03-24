77e8140524db829577652a1f761943c3ef162c2c
Korsar21 <114531160+Korsar21@users.noreply.github.com>
2025-03-24 18:23:52 +0300

    Удалены все файлы

--- main.cpp
+++ main.cpp
@@ -1,36 +1,105 @@
-#include <fstream>
 #include <iostream>
-#include "ticket/libTicket.hpp"
-#include "planet/libPlanet.hpp"
-#include "planet.hpp"
+#include <iomanip>
+#include "lib/fraction.h"
+#include "lib/fraction.cpp" // По требованию задания
+
+void demo() {
+    std::cout << "=== Демонстрационный режим ===" << std::endl;
+
+    // Создаем дробь для демонстрации
+    fraction z("3/4");
+    std::cout << "z=" << z << std::endl;
+
+    // Проверка конструкторов
+    fraction fr1(10, 14), fr2;
+    std::cout << "fr2=" << fr2 << std::endl;
+    std::cout << "fr1=" << fr1 << std::endl;
+    fraction fr = "-1 4/8";
+    std::cout << "fr=" << fr << std::endl;
+    fraction x(z), y;
+    std::cout << "x=" << x << std::endl;
+    double dbl = -1.25;
+    fraction f = dbl;
+    std::cout << "f=" << f << std::endl;
+
+    // Проверка перегруженной операции "+"
+    y = x + z;
+    std::cout << "y=" << y << std::endl;
+    y += x;
+    f += dbl / 2;
+    std::cout << "f=" << f << std::endl;
+    y = x + dbl;
+    std::cout << "y=" << y << std::endl;
+    y = dbl + y;
+    std::cout << "y=" << y << std::endl;
+    y += dbl;
+    std::cout << "y=" << y << std::endl;
+    int i = 5;
+    y += i;
+    std::cout << "y=" << y << std::endl;
+    y = i + x;
+    std::cout << "y=" << y << std::endl;
+    y = x + i;
+    std::cout << "y=" << y << std::endl;
+    y += dbl + i + x;
+    std::cout << "y=" << y << std::endl;
+}
+
+void interactive() {
+    // ввод дроби с клавиатуры
+    std::cout << "Введите дробь: \n";
+    fraction z;
+    std::cin >> z;
+    std::cout << "z=" << z << std::endl;
+
+    // проверка конструкторов
+    fraction fr1(10, 14), fr2;
+    std::cout << "fr2=" << fr2 << std::endl;
+    std::cout << "fr1=" << fr1 << std::endl;
+    fraction fr = "-1 4/8";
+    std::cout << "fr=" << fr << std::endl;
+    fraction x(z), y;
+    std::cout << "x=" << x << std::endl;
+    double dbl = -1.25;
+    fraction f = dbl;
+    std::cout << "f=" << f << std::endl;
+
+    // проверка перегруженной операции "+"
+    y = x + z;
+    std::cout << "y=" << y << std::endl;
+    y += x;
+    f += dbl / 2;
+    std::cout << "f=" << f << std::endl;
+    y = x + dbl;
+    std::cout << "y=" << y << std::endl;
+    y = dbl + y;
+    std::cout << "y=" << y << std::endl;
+    y += dbl;
+    std::cout << "y=" << y << std::endl;
+    int i = 5;
+    y += i;
+    std::cout << "y=" << y << std::endl;
+    y = i + x;
+    std::cout << "y=" << y << std::endl;
+    y = x + i;
+    std::cout << "y=" << y << std::endl;
+    y += dbl + i + x;
+    std::cout << "y=" << y << std::endl;
+}
 
 int main() {
-    int choice = choiceLib();
-    int count = 0;
-    Planet* planetDataBase = nullptr;
-    Ticket* ticketDataBase = nullptr;
-    const char* filename;
+    setlocale(LC_ALL, "Russian");
 
-    switch (choice) {
-        case 1: {
-            filename = "planets.txt";
-            planetSpace::switchPlanet(planetDataBase, count, filename);
-            break;
-        }
-        case 2: {
-            filename = "tickets.txt";
-            ticketSpace::switchTicket(ticketDataBase, count, filename);
-            break;
-        }
-        default:
-            std::cout << "Некорректный выбор.\n";
-            return 1;
-    }
+    // Выбор режима
+    char choice;
+    std::cout << "Выберите режим работы (d - демо, i - интерактивный): ";
+    std::cin >> choice;
+    std::cin.ignore(); // Очистка буфера ввода
 
-    if (choice == 1 && planetDataBase != nullptr) {
-        delete[] planetDataBase;
-    } else if (choice == 2 && ticketDataBase != nullptr) {
-        delete[] ticketDataBase;
+    if (choice == 'd' || choice == 'D') {
+        demo();
+    } else {
+        interactive();
     }
 
     return 0;
