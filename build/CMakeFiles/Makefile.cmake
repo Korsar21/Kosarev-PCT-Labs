3bd09a4a2c303ff14b122f6e69cefaac0942b9b4
Korsar21 <114531160+Korsar21@users.noreply.github.com>
2025-03-31 22:06:30 +0300

    add files

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -46,6 +46,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/FractionApp.dir/DependInfo.cmake"
+  "CMakeFiles/fraction_app.dir/DependInfo.cmake"
   "fractionLib/CMakeFiles/fraction.dir/DependInfo.cmake"
   )
